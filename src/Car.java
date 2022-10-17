78775a06134ad6f1a72d4df526eaffc923cf8595
Анастасия <fumi_nako@mail.ru>
2022-10-18 02:45:30 +0300

    OOП Задание 7

--- src/Car.java
+++ src/Car.java
@@ -7,13 +7,38 @@ public class Car {
     private final String productionCountry;
 
     Car (String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.productionYear = productionYear;
-        this.productionCountry = productionCountry;
+        if (brand.equals("")) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model.equals("")) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume<0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color.equals("")) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (productionYear < 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        if (productionCountry.equals("")) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
     }
+
 
     public String toString() {
         return brand + " " + model + ", " + productionYear + " год выпуска, сборка " + productionCountry + ", " + color + " цвет кузова, объем двигателя — " + engineVolume + " л.";
